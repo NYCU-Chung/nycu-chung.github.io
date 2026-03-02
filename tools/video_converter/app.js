e6e90989eced75cf94efca54cb498d79d5101e46
NYCU-Chung <chung.la13@nycu.edu.tw>
2026-03-02 16:54:50 +0800

    fix: use single-threaded FFmpeg core for GitHub Pages

--- tools/video_converter/app.js
+++ tools/video_converter/app.js
@@ -75,7 +75,7 @@ async function initFFmpeg() {
 
         ffmpeg = createFFmpeg({
             log: true,
-            corePath: 'https://unpkg.com/@ffmpeg/core@0.11.0/dist/ffmpeg-core.js',
+            corePath: 'https://unpkg.com/@ffmpeg/core-st@0.11.1/dist/ffmpeg-core.js',
         });
 
         ffmpeg.setProgress(({ ratio }) => {
